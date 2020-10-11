b390f285e2430a6ea74cffa0187ee0468ae829fb
bamerf <erfanianbamdad@gmail.com>
2020-10-11 20:09:59 +1100

    added validator function

---
+++ validators.js
@@ -0,0 +1,21 @@
+exports.isValidName = (name) => {
+	// at least 3 letters
+	// remove whitespace
+	if (typeof name == "string" && name.trim().length >= 3) {
+		return true;
+	}
+	return false;
+};
+
+exports.isValidNumber = (input) => {
+	// number or string number
+	// whole number
+	// no negative numbers
+	if (typeof input == "string" && input.trim() != "") {
+		input = Number(input);
+	}
+	if (typeof input === "number" && input >= 0 && Number.isInteger(input)) {
+		return true;
+	}
+	return false;
+};
